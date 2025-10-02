9833ef863ea3c79c284c860c661d22a930567ca7
Shreenath <shreenath2k4@gmail.com>
2025-10-02 20:59:06 +0530

    Initial commit: Flash To-Do app

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+# Database model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
+
+# Home page
+@app.route('/')
+def index():
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+# Add a new task
+@app.route('/add', methods=['POST'])
+def add():
+    task_content = request.form['content']
+    new_task = Task(content=task_content)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# Mark as done/undo
+@app.route('/done/<int:id>')
+def done(id):
+    task = Task.query.get_or_404(id)
+    task.done = not task.done
+    db.session.commit()
+    return redirect(url_for('index'))
+
+# Delete a task
+@app.route('/delete/<int:id>')
+def delete(id):
+    task = Task.query.get_or_404(id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
+
